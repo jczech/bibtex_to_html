dc2a4af0e28fd81da09b99c9ef0fa145f4fcb59e
Jacob Czech <czech.jacob@gmail.com>
2017-02-06 12:30:45 -0500

    Format how the parsed data is stored.
    Use setParseAction to store data in a format that will be easier to
    convert into html.

--- pyparsing_test.py
+++ pyparsing_test.py
@@ -18,39 +18,41 @@ wrapped = lbr + sentence + rbr + pp.Optional(comma)
 wrapped_title = lbr + lbr + sentence + rbr + rbr + pp.Optional(comma)
 wrapped_num = lbr + pp.Word(pp.nums) + rbr
 
-authors = "author" + eq + wrapped
-doi = "doi" + eq + wrapped
-journal = "journal" + eq + wrapped
-keywords = "keywords" + eq + wrapped
-mendeley = "mendeley-tags" + eq + wrapped
-number = "number" + eq + wrapped_num + comma # issue
-pages = "pages" + eq + wrapped
-pmid = "pmid" + eq + wrapped
-publisher = "publisher" + eq + wrapped
-title = "title" + eq + wrapped_title
-url = "url" + eq + wrapped
-volume = "volume" + eq + wrapped_num + comma
-year = "year" + eq + wrapped_num
+authors = ("author" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+doi = ("doi" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+journal = ("journal" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+keywords = ("keywords" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+mendeley = ("mendeley-tags" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+number = ("number" + eq + wrapped_num + comma).setParseAction(lambda s, l, t: (t[0], t[1][0])) # issue
+pages = ("pages" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+pmid = ("pmid" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+publisher = ("publisher" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+title = ("title" + eq + wrapped_title).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+url = ("url" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
+volume = ("volume" + eq + wrapped_num + comma).setParseAction(lambda s, l, t: (t[0], t[1]))
+year = ("year" + eq + wrapped_num).setParseAction(lambda s, l, t: (t[0], t[1]))
 other = pp.Word(pp.alphas) + eq + pp.restOfLine()
 
 unit = (authors | year | title | journal | volume | number | pages | doi |
         pmid | mendeley | url | pp.Suppress(other))
 
 entry = pp.OneOrMore(
-    (article_start | inproceedings) + lbr + author_year + pp.OneOrMore(unit) + rbr)
+    (article_start | inproceedings) + lbr + author_year + pp.OneOrMore(unit) +
+    rbr)
 
 def main():
     with open("mmbios.bib", "r") as bib_file:
         lines = bib_file.read()
         data = entry.parseString(lines)
         for d in data:
-            if type(d) == str:
-                if d.startswith("@"):
-                    print("-"*40)
-                else:
-                    print(d)
-            else:
-                print(d[0])
+            print(d)
+            # if type(d) == str:
+            #     if d.startswith("@"):
+            #         print("-"*40)
+            #     else:
+            #         print(d)
+            # else:
+            #     print(d[0])
 
 
 if __name__ == "__main__":
