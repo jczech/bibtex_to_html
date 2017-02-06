fce1f706a95b69a761180f31b94a30bca08a48cf
Jacob Czech <czech.jacob@gmail.com>
2017-02-06 11:38:19 -0500

    Cleanup output. Remove unneeded elements.

--- pyparsing_test.py
+++ pyparsing_test.py
@@ -4,8 +4,8 @@ import pyparsing as pp
 from string import lowercase
 from pprint import pprint
 
-article_start = pp.Suppress("@article")
-author_year = pp.Word(pp.alphanums)
+article_start = pp.Word("@article")
+author_year = pp.Suppress(pp.Word(pp.alphanums))
 comma = pp.Suppress(",")
 eq = pp.Suppress("=")
 
@@ -17,39 +17,26 @@ wrapped = lbr + sentence + rbr + pp.Optional(comma)
 wrapped_title = lbr + lbr + sentence + rbr + rbr + pp.Optional(comma)
 wrapped_num = lbr + pp.Word(pp.nums) + rbr
 
-abstract = pp.Suppress("abstract" + eq + wrapped)
-author = "author" + eq + wrapped
+authors = "author" + eq + wrapped
 doi = "doi" + eq + wrapped
-eprint = "eprint" + eq + wrapped
-isbn = "isbn" + eq + wrapped
-issn = "issn" + eq + wrapped
 journal = "journal" + eq + wrapped
 keywords = "keywords" + eq + wrapped
 mendeley = "mendeley-tags" + eq + wrapped
-month = "month" + eq + wrapped
 number = "number" + eq + wrapped_num + comma # issue
 pages = "pages" + eq + wrapped
 pmid = "pmid" + eq + wrapped
 publisher = "publisher" + eq + wrapped
 title = "title" + eq + wrapped_title
 url = "url" + eq + wrapped
-other = pp.Word(pp.alphas) + eq + wrapped
 volume = "volume" + eq + wrapped_num + comma
 year = "year" + eq + wrapped_num
+other = pp.Word(pp.alphas) + eq + wrapped
 
-unit = (author | year | title | journal | volume | number | pages | doi |
-        pmid | mendeley | pp.Suppress(other))
+unit = (authors | year | title | journal | volume | number | pages | doi |
+        pmid | mendeley | url | pp.Suppress(other))
 
 entry = pp.OneOrMore(
     article_start + lbr + author_year + comma + pp.OneOrMore(unit) + rbr)
-
-# entry = pp.OneOrMore(
-#     article_start + lbr + author_year + comma + abstract + author + doi +
-#     pp.Optional(eprint) + isbn + pp.Optional(issn) + pp.Optional(journal) +
-#     pp.Optional(keywords) + pp.Optional(mendeley) + month + pp.Optional(number)
-#     + pages + pp.Optional(pmid) + pp.Optional(publisher) + title + url +
-#     pp.Optional(volume) + year + rbr)
-
 
 def main():
     with open("mmbios.bib", "r") as bib_file:
@@ -58,7 +45,10 @@ def main():
         data = entry.parseString(lines)
         for d in data:
             if type(d) == str:
-                print(d)
+                if d == "@article":
+                    print("-"*40)
+                else:
+                    print(d)
             else:
                 print(d[0])
 
