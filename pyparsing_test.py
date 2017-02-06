7c0195bc3cd9b7e7e7e34de759df82ffbbeaec9a
Jacob Czech <czech.jacob@gmail.com>
2017-02-06 16:39:48 -0500

    Make a list of dictionaries for articles.
    This makes it a little easier when we need to access multiple elements
    simultaneously (like you need to do with the URL and title).

--- pyparsing_test.py
+++ pyparsing_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pyparsing as pp
+import re
 from string import lowercase
 from pprint import pprint
 
@@ -16,7 +17,9 @@ link_dict = {
     "CSP":  "research/collaboration-service",
 }
 
-article_start = pp.Literal("@article")
+articles = []
+
+article_start = pp.Literal("@article").setParseAction(lambda s, l, t: articles.append({}))
 inproceedings = pp.Literal("@inproceedings")
 author_year = pp.Suppress(pp.restOfLine)
 comma = pp.Suppress(",")
@@ -27,21 +30,35 @@ rbr = pp.Suppress("}")
 atom = pp.CharsNotIn("{}") 
 sentence = pp.Group(pp.OneOrMore(atom))
 wrapped = lbr + sentence + rbr + pp.Optional(comma)
+wrapped_authors = lbr + pp.restOfLine
 wrapped_title = lbr + lbr + sentence + rbr + rbr + pp.Optional(comma)
 wrapped_num = lbr + pp.Word(pp.nums) + rbr
 
-authors = ("author" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-doi = ("doi" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-journal = ("journal" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-mendeley = ("mendeley-tags" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-number = ("number" + eq + wrapped_num + comma).setParseAction(lambda s, l, t: (t[0], t[1][0])) # issue
-pages = ("pages" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-pmid = ("pmid" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-publisher = ("publisher" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-title = ("title" + eq + wrapped_title).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-url = ("url" + eq + wrapped).setParseAction(lambda s, l, t: (t[0], t[1][0]))
-volume = ("volume" + eq + wrapped_num + comma).setParseAction(lambda s, l, t: (t[0], t[1]))
-year = ("year" + eq + wrapped_num).setParseAction(lambda s, l, t: (t[0], t[1]))
+def save_author(s, l, t):
+    # Ugh. Need to strip off trailing "}," because I'm not sure how to handle
+    # the curly braces nested inside of authors list 
+    # (e.g author = {Czech, Jacob and Name, We{\^{i}}t}rd})
+    articles[-1][t[0]] = t[1][:-2] 
+    # articles[-1][t[0]] = re.sub('', '', t[1][:-2])
+
+def save_list_entry(s, l, t):
+    articles[-1][t[0]] = t[1][0]
+
+def save_string_entry(s, l, t):
+    articles[-1][t[0]] = t[1]
+
+authors = ("author" + eq + wrapped_authors).setParseAction(save_author)
+doi = ("doi" + eq + wrapped).setParseAction(save_list_entry)
+journal = ("journal" + eq + wrapped).setParseAction(save_list_entry)
+mendeley = ("mendeley-tags" + eq + wrapped).setParseAction(save_list_entry)
+number = ("number" + eq + wrapped_num + comma).setParseAction(save_list_entry) # issue
+pages = ("pages" + eq + wrapped).setParseAction(save_list_entry)
+pmid = ("pmid" + eq + wrapped).setParseAction(save_list_entry)
+publisher = ("publisher" + eq + wrapped).setParseAction(save_list_entry)
+title = ("title" + eq + wrapped_title).setParseAction(save_list_entry)
+url = ("url" + eq + wrapped).setParseAction(save_list_entry)
+volume = ("volume" + eq + wrapped_num + comma).setParseAction(save_string_entry)
+year = ("year" + eq + wrapped_num).setParseAction(save_string_entry)
 other = pp.Word(pp.alphas) + eq + pp.restOfLine
 
 unit = (authors | year | title | journal | volume | number | pages | doi |
@@ -75,16 +92,20 @@ def main():
     with open("mmbios.bib", "r") as bib_file:
         lines = bib_file.read()
         data = entry.parseString(lines)
-        for d in data:
-            if type(d) == str:
-                print("-"*40)
-            else:
-                if d[0] == "author":
-                    print(get_authors(d[1]))
-                elif d[0] == "year":
-                    print("<span class=\"pubdate\">(%s) </span>" % d[1])
-                else:
-                    print(d[1])
+        # print(data)
+        for article in articles:
+            pprint(article)
+            author = get_authors(article["author"])
+            # if type(d) == str:
+            #     print("-"*40)
+            # else:
+            #     if d[0] == "author":
+            #         print(get_authors(d[1]))
+            #     elif d[0] == "year":
+            #         print("<span class=\"pubdate\">(%s) </span>" % d[1])
+            #     else:
+            #         print(d)
+            #         # print(d[1])
 
 
 if __name__ == "__main__":
